8132d16f18219245beff0eb3ff4fc0396d3d468c
Fdom92 <fer.olmo92@gmail.com>
2016-10-08 12:35:20 +0200

    Deleted initial pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,6 @@ import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from 'ionic-native';
 
-import { Page1 } from '../pages/page1/page1';
-import { Page2 } from '../pages/page2/page2';
 import { pantryList } from '../pages/pantryList/pantryList';
 
 
@@ -13,7 +11,7 @@ import { pantryList } from '../pages/pantryList/pantryList';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = Page1;
+  rootPage: any = pantryList;
 
   pages: Array<{title: string, component: any}>;
 
@@ -22,8 +20,6 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Page One', component: Page1 },
-      { title: 'Page Two', component: Page2 },
       { title: 'List', component: pantryList }
     ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicApp, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { Page1 } from '../pages/page1/page1';
-import { Page2 } from '../pages/page2/page2';
+
 import { pantryList } from '../pages/pantryList/pantryList';
 import { Snacks } from '../pages/pantryList/tabs/snacksTab';
 import { Drinks } from '../pages/pantryList/tabs/drinksTab';
@@ -11,8 +10,6 @@ import { Frozen } from '../pages/pantryList/tabs/frozenTab';
 @NgModule({
   declarations: [
     MyApp,
-    Page1,
-    Page2,
     pantryList,
     Snacks,
     Drinks,
@@ -24,8 +21,6 @@ import { Frozen } from '../pages/pantryList/tabs/frozenTab';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    Page1,
-    Page2,
     pantryList,
     Snacks,
     Drinks,
