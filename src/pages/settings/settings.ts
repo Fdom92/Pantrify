e2221060c503a99ccc73323ccdca0d117482a317
Fernando del olmo <fer.olmo92@gmail.com>
2016-11-28 15:10:01 +0100

    [internationalization] POC

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,12 +1,20 @@
 import {Component} from "@angular/core";
-import { NavController } from 'ionic-angular';
+import {TranslateService} from 'ng2-translate';
 
 @Component({
     templateUrl: "settings.html"
 })
 export class Settings {
 
-    constructor(public navCtrl: NavController) {
+    constructor(public translate: TranslateService) {
+        // this language will be used as a fallback when a translation isn't found in the current language
+        translate.setDefaultLang('en');
+
+         // the lang to use, if the lang isn't available, it will use the current loader to get them
+        translate.use('en');
     }
 
+    onChange(e) {
+        this.translate.use(e);
+    }
 }
