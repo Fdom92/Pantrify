46e561ee9c0e6ebc16af5b6720b85d1b65860824
Fernando del olmo <fer.olmo92@gmail.com>
2016-11-30 13:09:33 +0100

    Refactor sidemenu translations

--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -1,6 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
-import { TranslateService, LangChangeEvent } from 'ng2-translate';
 import { PantryList } from '../pantryList/pantryList';
 import { Settings } from '../settings/settings';
 import { Help } from '../help/help';
@@ -18,21 +17,14 @@ export class Menu {
   pages: Array<{title: string, component: any, icon: string}>;
   pages2: Array<{title: string, component: any, icon: string}>;
 
-  constructor(public platform: Platform, public translate: TranslateService) {
-    this.pages = [{ title: 'Pantry', component: PantryList, icon: 'home' },
-                  { title: 'Inventory', component: Inventory, icon: 'clipboard' }];
+  constructor(public platform: Platform) {
+    this.pages = [{ title: 'pantry', component: PantryList, icon: 'home' },
+                  { title: 'inventory', component: Inventory, icon: 'clipboard' }];
 
     this.pages2 = [{ title: 'Settings', component: Settings, icon: 'settings' },
       { title: 'Send feedback', component: Feedback, icon: 'text' },
       { title: 'Help', component: Help, icon: 'help-circle' }];
 
-    this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
-      this.pages[0].title = event.translations.Menu.pantry;
-      this.pages[1].title = event.translations.Menu.inventory;
-      this.pages2[0].title = event.translations.Menu.settings;
-      this.pages2[1].title = event.translations.Menu.sendFeedback;
-      this.pages2[2].title = event.translations.Menu.help;
-    });
   }
 
   openPage(page) {
