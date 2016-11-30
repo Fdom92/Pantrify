df4b43cdfdea1940d29dcd47dd1009feb159292e
Fernando del olmo <fer.olmo92@gmail.com>
2016-11-30 09:42:13 +0100

    Moved ng2-translate conf into app componet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
+import {TranslateService} from 'ng2-translate';
 import { StatusBar } from 'ionic-native';
 import { Home } from '../pages/home/home';
 
@@ -10,11 +11,16 @@ export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
   rootPage: any = Home;
-  constructor(public platform: Platform) {
+  constructor(public platform: Platform, public translate: TranslateService) {
     this.initializeApp();
   }
 
   initializeApp() {
+    // this language will be used as a fallback when a translation isn't found in the current language
+    this.translate.setDefaultLang('en');
+
+      // the lang to use, if the lang isn't available, it will use the current loader to get them
+    this.translate.use('en');
 
     this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -7,11 +7,7 @@ import {TranslateService} from 'ng2-translate';
 export class Settings {
 
     constructor(public translate: TranslateService) {
-        // this language will be used as a fallback when a translation isn't found in the current language
-        translate.setDefaultLang('en');
 
-         // the lang to use, if the lang isn't available, it will use the current loader to get them
-        translate.use('en');
     }
 
     onChange(e) {
