2cce287fb0b01d32edd26c3ed435898e18add3b3
Fernando del olmo <fer.olmo92@gmail.com>
2016-11-30 09:42:30 +0100

    Added translations into sidemenu

--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
-import { Nav, Platform, ModalController } from 'ionic-angular';
+import { Nav, Platform } from 'ionic-angular';
+import { TranslateService, LangChangeEvent } from 'ng2-translate';
 import { PantryList } from '../pantryList/pantryList';
 import { Settings } from '../settings/settings';
 import { Help } from '../help/help';
@@ -17,13 +18,21 @@ export class Menu {
   pages: Array<{title: string, component: any, icon: string}>;
   pages2: Array<{title: string, component: any, icon: string}>;
 
-  constructor(public platform: Platform, public modalCtrl: ModalController) {
+  constructor(public platform: Platform, public translate: TranslateService) {
     this.pages = [{ title: 'Pantry', component: PantryList, icon: 'home' },
                   { title: 'Inventory', component: Inventory, icon: 'clipboard' }];
 
     this.pages2 = [{ title: 'Settings', component: Settings, icon: 'settings' },
       { title: 'Send feedback', component: Feedback, icon: 'text' },
       { title: 'Help', component: Help, icon: 'help-circle' }];
+
+    this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
+      this.pages[0].title = event.translations.Menu.pantry;
+      this.pages[1].title = event.translations.Menu.inventory;
+      this.pages2[0].title = event.translations.Menu.settings;
+      this.pages2[1].title = event.translations.Menu.sendFeedback;
+      this.pages2[2].title = event.translations.Menu.help;
+    });
   }
 
   openPage(page) {
