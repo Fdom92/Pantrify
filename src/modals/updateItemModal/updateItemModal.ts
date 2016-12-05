9b0c3c4904fa23f2ec8d17a974861d9cf3f44d59
Fernando del olmo <fer.olmo92@gmail.com>
2016-12-05 16:55:31 +0100

    [Update Item] Updated modal

--- src/modals/updateItemModal/updateItemModal.ts
+++ src/modals/updateItemModal/updateItemModal.ts
@@ -6,28 +6,12 @@ import { NavParams, ViewController } from 'ionic-angular';
 })
 export class UpdateItemModal {
 
-  simpleColumns: any;
   counter: number;
   product: any;
 
  constructor(public viewCtrl: ViewController, params: NavParams) {
    this.product = params.get('product');
    this.counter = params.get('product').units;
-   this.simpleColumns = [{
-      name: 'col1',
-      options: [
-        { text: '0', value: '0' },
-        { text: '1', value: '1' },
-        { text: '2', value: '2' },
-        { text: '3', value: '3' },
-        { text: '4', value: '4' },
-        { text: '5', value: '5' },
-        { text: '6', value: '6' },
-        { text: '7', value: '7' },
-        { text: '8', value: '8' },
-        { text: '9', value: '9' },
-        { text: '10', value: '10' }]
-  }];
  }
 
  dismiss() {
@@ -38,4 +22,13 @@ export class UpdateItemModal {
    this.viewCtrl.dismiss({ product: this.product, units: this.counter});
  }
 
+ onAdd() {
+   this.counter++;
+ }
+
+ onRemove() {
+   if (this.counter > 0) {
+     this.counter--;
+   }
+ }
 }
