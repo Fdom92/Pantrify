04ef6a33a0bf555c48c057c94524ee57e5a287fa
Fernando del olmo <fer.olmo92@gmail.com>
2017-01-02 15:48:27 +0100

    [App] Refactor II

--- src/pages/inventory/inventory.ts
+++ src/pages/inventory/inventory.ts
@@ -10,18 +10,9 @@ import { AddItemModal } from '../../modals/addItemModal/addItemModal';
     templateUrl: "inventory.html"
 })
 export class InventoryPage {
-
     food: FirebaseListObservable<any[]>;
     home:FirebaseListObservable<any[]>;
     drink: FirebaseListObservable<any[]>;
-    simpleColumns = [{
-        name: 'col1',
-        options: [
-        { text: '0', value: '0' },{ text: '1', value: '1' },{ text: '2', value: '2' },
-        { text: '3', value: '3' },{ text: '4', value: '4' },{ text: '5', value: '5' },
-        { text: '6', value: '6' },{ text: '7', value: '7' },{ text: '8', value: '8' },
-        { text: '9', value: '9' },{ text: '10', value: '10' }]
-    }];
 
     constructor(public modalCtrl: ModalController, public navCtrl: NavController, af: AngularFire) {
       this.food = af.database.list('/food');
