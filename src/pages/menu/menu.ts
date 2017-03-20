3c4b445f520c15a53d7d1bc15cab072a9fee5bde
Fernando del olmo martin <fer.olmo92@gmail.com>
2017-03-20 11:22:22 +0100

    Added MD style sidemenu

--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -4,6 +4,7 @@ import { Nav, Platform } from 'ionic-angular';
 
 import { PantryPage }    from '../pantry/pantry';
 import { SettingsPage }  from '../settings/settings';
+import { UserData } from '../../providers/user.provider';
 
 @Component({
   templateUrl: 'menu.html'
@@ -12,10 +13,11 @@ export class Menu {
   @ViewChild(Nav) nav: Nav;
 
   rootPage: any = PantryPage;
-
+  usermail: String;
   pages: Array<{title: string, component: any, iconMD: string, iconOS: string}>;
 
-  constructor(public platform: Platform) {
+  constructor(public platform: Platform, public userdata: UserData) {
+    this.usermail = this.userdata.getEmail();
     this.pages = [{ title: 'pantry', component: PantryPage, iconMD: 'md-home', iconOS: 'ios-home-outline' },
                   { title: 'settings', component: SettingsPage, iconMD: 'md-settings', iconOS: 'ios-settings-outline' }];
   }
